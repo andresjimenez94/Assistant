4328b19f114c600206d8f4cebdc76cf48d3aefab
Andres Jimenez Castro <ajmenez@veritran.com>
2023-11-29 10:41:14 -0500

    Creacion mas casos test

--- test/test_andrea.py
+++ test/test_andrea.py
@@ -1,10 +1,25 @@
 import unittest
-from utilities import utilities
+import utilities
 
 class TestUtilities(unittest.TestCase):
     
-     def test_afeccion_sana(self):
-         """ Prueba cuando una afeccion es sana"""
-         Valor = 330
-         result = utilities.nivelAfeccion(Valor)
-         self.assertAlmostEqual("sano", result)
+    def test_afeccion_sana(self):
+        """ Prueba cuando una afeccion es sana"""
+        Valor = 330
+        result = utilities.nivelAfeccion(Valor)
+        print(result)
+        self.assertAlmostEqual("sano", result)
+
+    def test_afeccion_enfermo(self):
+        """ Prueba cuando una afeccion es enfermo"""
+        Valor = 521
+        result = utilities.nivelAfeccion(Valor)
+        print(result)
+        self.assertAlmostEqual("enfermo", result)
+    
+    def test_afeccion_grave(self):
+        """ Prueba cuando una afeccion es grave"""
+        Valor = 1100
+        result = utilities.nivelAfeccion(Valor)
+        print(result)
+        self.assertAlmostEqual("grave", result)
