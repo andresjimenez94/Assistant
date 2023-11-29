879d921bc8360dfb24fe27ee9368664687fb7983
Andres Jimenez Castro <ajmenez@veritran.com>
2023-11-29 10:44:43 -0500

    Se quita print

--- test/test_andrea.py
+++ test/test_andrea.py
@@ -7,19 +7,16 @@ class TestUtilities(unittest.TestCase):
         """ Prueba cuando una afeccion es sana"""
         Valor = 330
         result = utilities.nivelAfeccion(Valor)
-        print(result)
         self.assertAlmostEqual("sano", result)
 
     def test_afeccion_enfermo(self):
         """ Prueba cuando una afeccion es enfermo"""
         Valor = 521
         result = utilities.nivelAfeccion(Valor)
-        print(result)
         self.assertAlmostEqual("enfermo", result)
     
     def test_afeccion_grave(self):
         """ Prueba cuando una afeccion es grave"""
         Valor = 1100
         result = utilities.nivelAfeccion(Valor)
-        print(result)
         self.assertAlmostEqual("grave", result)
